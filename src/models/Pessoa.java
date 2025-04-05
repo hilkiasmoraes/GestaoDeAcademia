530716466151d97e704b87f9b6186bfb30ece6c2
hilkiasmoraes <hilkias.moraes@gmail.com>
2025-04-05 14:29:11 -0300

    feat: Senai Java Projeto Academia

---
+++ src/models/Pessoa.java
@@ -0,0 +1,21 @@
+package models;
+
+abstract class Pessoa {
+    protected String nomePessoa;
+    protected int idadePessoa;
+
+    public Pessoa(String nome, int idade) {
+        this.nomePessoa = nome;
+        this.idadePessoa = idade;
+    }
+
+    public String getNomePessoa() {
+        return nomePessoa;
+    }
+
+    public int getIdadePessoa() {
+        return idadePessoa;
+    }
+
+    public abstract String getDescricao();
+}
